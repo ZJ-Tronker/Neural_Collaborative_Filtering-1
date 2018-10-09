5453b7d19fe9bb180cd9dacc5030d331fb28ebd5
TuneHwi Yoon <1074933893@qq.com>
2018-10-09 15:34:24 +0800

    Initialization
    NCF use tensorflow 1.7 &  keras 2.1.4

---
+++ Dataset.py
@@ -0,0 +1,68 @@
+'''
+Created on Aug 8, 2016
+Processing datasets. 
+
+@author: Xiangnan He (xiangnanhe@gmail.com)
+'''
+import scipy.sparse as sp
+import numpy as np
+
+
+class Dataset(object):
+
+    def __init__(self, path):
+
+        self.trainMatrix = self.load_rating_file_as_matrix(path + ".train.rating")
+        self.testRatings = self.load_rating_file_as_list(path + ".test.rating")
+        self.testNegatives = self.load_negative_file(path + ".test.negative")
+        assert len(self.testRatings) == len(self.testNegatives)
+        self.num_users, self.num_items = self.trainMatrix.shape
+
+    # get the interaction matrix of user-item
+    def load_rating_file_as_matrix(self, filename):
+
+        # Get number of users and items
+        num_users, num_items = 0, 0
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line is not None and line != "":
+                arr = line.split("\t")
+                u, i = int(arr[0]), int(arr[1])
+                num_users = max(num_users, u)
+                num_items = max(num_items, i)
+                line = f.readline()
+        # Construct matrix
+        mat = sp.dok_matrix((num_users + 1, num_items + 1), dtype=np.float32)
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line is not None and line != "":
+                arr = line.split("\t")
+                user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
+                if rating > 0:
+                    mat[user, item] = 1.0
+                line = f.readline()
+        return mat
+
+    def load_rating_file_as_list(self, filename):
+        ratingList = []
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line is not None and line != "":
+                arr = line.split("\t")
+                user, item = int(arr[0]), int(arr[1])
+                ratingList.append([user, item])
+                line = f.readline()
+        return ratingList
+
+    def load_negative_file(self, filename):
+        negativeList = []
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line is not None and line != "":
+                arr = line.split("\t")
+                negatives = []
+                for x in arr[1:]:
+                    negatives.append(int(x))
+                negativeList.append(negatives)
+                line = f.readline()
+        return negativeList
